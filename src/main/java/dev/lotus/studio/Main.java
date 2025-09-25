4f76a6a7ca6927ff176a507b79cbb9a76b7dc8cf
Referix <ostap.salo.2017@gmail.com>
2025-09-25 15:59:13 +0300

    hotFix Metrics

--- src/main/java/dev/lotus/studio/Main.java
+++ src/main/java/dev/lotus/studio/Main.java
@@ -3,7 +3,7 @@ package dev.lotus.studio;
 import dev.lotus.studio.database.DatabaseInitializer;
 import dev.lotus.studio.database.playerdata.PlayerDataService;
 import dev.lotus.studio.safezone.SafeZonePreloaded;
-import org.bstats.bukkit.Metrics;
+import dev.lotus.studio.utils.metric.Metrics;
 import org.bukkit.event.HandlerList;
 import org.bukkit.plugin.java.JavaPlugin;
 import dev.lotus.studio.database.savezone.SafeZoneDataService;
